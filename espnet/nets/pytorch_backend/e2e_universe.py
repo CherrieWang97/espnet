81d5a723493c5e421263ff666319a9443d000044
cywang <cywang@nankai.edu.cn>
2019-07-13 15:50:55 +0800

    add setting3

--- espnet/nets/pytorch_backend/e2e_universe.py
+++ espnet/nets/pytorch_backend/e2e_universe.py
@@ -97,13 +97,14 @@ class E2E(ASRInterface, torch.nn.Module):
 
 
         self.frontend = None
-        self.prenet = PreNet(idim, 3, args.eunits, args.dropout_rate)
+        self.prenet = PreNet(idim, 2, args.eunits, args.dropout_rate)
         self.dropout_pre = torch.nn.Dropout(p=args.dropout_rate)
+        self.linear_pre = torch.nn.Linear(args.eunits*2, args.eunits)
         self.ctc = ctc_for(args, odim)
         # encoder
         self.embed_src = torch.nn.Embedding(odim, args.eunits, padding_idx=self.eos, _weight=self.ctc.ctc_lo.weight)
         self.dropemb = torch.nn.Dropout(p=args.dropout_rate)
-        self.enc = Encoder('blstm', args.eunits, 2, args.eunits, args.eprojs, subsample, dropout=args.dropout_rate)
+        self.enc = Encoder('blstm', args.eunits, 3, args.eunits, args.eprojs, subsample, dropout=args.dropout_rate)
         # attention
         self.att = att_for(args)
         # decoder
@@ -236,6 +237,8 @@ class E2E(ASRInterface, torch.nn.Module):
         # 0. prenet
         if task == "st" or task == "asr":
             hs_pad, hlens, _ = self.prenet(xs_pad, ilens)
+            hs_pad = self.dropout_pre(hs_pad)
+            hs_pad = self.linear_pre(hs_pad)
             hs_pad = self.dropout_pre(hs_pad)
         else:
             hs_pad = self.dropemb(self.embed_src(xs_pad))
