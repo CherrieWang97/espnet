b9f049493f767a49e1ff9c64a02ee1161b39ef7d
cywang <cywang@mail.nankai.edu.cn>
2019-07-30 11:32:57 +0800

    alpha

--- espnet/nets/pytorch_backend/e2e_universe.py
+++ espnet/nets/pytorch_backend/e2e_universe.py
@@ -98,35 +98,35 @@ class E2E(ASRInterface, torch.nn.Module):
 
         self.frontend = None
         self.prenet = PreNet(idim, 2, args.eunits, args.dropout_rate)
-        self.ctc = ctc_for(args, odim)
+        self.ctc = ctc_for(args, 10002)
         # encoder
-        self.embed_src = torch.nn.Embedding(odim, args.eunits, padding_idx=self.eos, _weight=self.ctc.ctc_lo.weight)
+        self.embed_src = torch.nn.Embedding(10002, args.eunits, padding_idx=self.eos, _weight=self.ctc.ctc_lo.weight)
         self.dropemb = torch.nn.Dropout(p=args.dropout_rate)
         self.enc = Encoder('blstm', args.eunits, 3, args.eunits, args.eprojs, subsample, dropout=args.dropout_rate)
         # attention
         self.att = att_for(args)
         # decoder
-        self.dec = decoder_for(args, odim, self.sos, self.eos, self.att, labeldist, embed=self.embed_src)
-
+        self.dec = decoder_for(args, odim, self.sos, self.eos, self.att, labeldist)
         # weight initialization
         self.init_like_chainer()
 
         # pre-training w/ ASR encoder and NMT decoder
-        if asr_model is not None:
-            param_dict = dict(asr_model.named_parameters())
-            for n, p in self.named_parameters():
-                # overwrite the encoder
-                if n in param_dict.keys() and p.size() == param_dict[n].size():
-                    p.data = param_dict[n].data
-                    logging.warning('Overwrite %s' % n)
         if mt_model is not None:
             param_dict = dict(mt_model.named_parameters())
             for n, p in self.named_parameters():
                 # overwrite the decoder
                 if n in param_dict.keys() and p.size() == param_dict[n].size():
-                    if 'dec.' in n or 'att' in n:
+                    if 'enc.' in n or'dec.' in n or 'att' in n:
                         p.data = param_dict[n].data
-                        logging.warning('Overwrite %s' % n)
+                        logging.warning('Overwrite %s from mt model' % n)
+        if asr_model is not None:
+            param_dict = dict(asr_model.named_parameters())
+            for n, p in self.named_parameters():
+                # overwrite the encoder
+                if n in param_dict.keys() and p.size() == param_dict[n].size():
+                    if 'prenet' in n or 'ctc' in n :
+                        p.data = param_dict[n].data
+                        logging.warning('Overwrite %s from asr model' % n)
         if st_model is not None:
             param_dict = dict(st_model.named_parameters())
             for n, p in self.named_parameters():
@@ -252,8 +252,12 @@ class E2E(ASRInterface, torch.nn.Module):
 
         cs_pad, clens, _ = self.enc(hs_pad, hlens)
 
-
-        loss_att, acc, ppl = self.dec(cs_pad, clens, ys_pad, tgt_lang_ids=tgt_lang_ids)
+        if task == "asr" and self.mtalpha == 1.0:
+            loss_att = 0.0
+            ppl = 0.0
+            acc = 0.0
+        else:
+            loss_att, acc, ppl = self.dec(cs_pad, clens, ys_pad, tgt_lang_ids=tgt_lang_ids)
 
         # 4. compute cer without beam search
         if task == "asr":
